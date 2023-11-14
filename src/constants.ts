352f67f7ecaf68ce905c8dea649206609fbecf70
gioboa <giorgiob.boa@gmail.com>
2023-11-14 10:09:01 +0100

    feat: add uniqueId for testing

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import { createContextId } from '@builder.io/qwik';
 import { AppState } from './types';
 
 export const APP_STATE = createContextId<AppState>('app_state');
-export const AUTH_TOKEN = 'authToken';
+export const AUTH_TOKEN = 'authToken' + (Math.random() * 100).toFixed(0);
 export const CUSTOMER_NOT_DEFINED_ID = 'CUSTOMER_NOT_DEFINED_ID';
 export const HEADER_AUTH_TOKEN_KEY = 'vendure-auth-token';
 export const IMAGE_RESOLUTIONS = [1000, 800, 600, 400];
